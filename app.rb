41acdc1ef705fd3c5dd1b2c25947bc99e2120a2e
Grace Mekarski & Mike Goren <student@epicodus.com>
2015-01-28 10:16:31 -0800

    add sinatra interface for adding surveys and corresponding questions

--- app.rb
+++ app.rb
@@ -2,3 +2,31 @@ require("bundler/setup")
 Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get("/") do
+  erb(:index)
+end
+
+post("/surveys") do
+  name = params.fetch('name')
+  Survey.create({:name => name})
+  redirect('/')
+end
+
+get("/surveys/:id") do
+  @survey_id = params.fetch('id').to_i()
+  erb(:survey)
+end
+
+post("/questions") do
+  question = params.fetch('question')
+  survey_id = params.fetch('survey_id').to_i()
+  Question.create({ :question => question, :survey_id => survey_id })
+  url = "/surveys/" + survey_id.to_s()
+  redirect(url)
+end
+
+get("/questions/:id") do
+  @question_id = params.fetch('id').to_i()
+  erb(:question)
+end
